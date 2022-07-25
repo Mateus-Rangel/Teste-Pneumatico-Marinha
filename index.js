b60237ab45a05ce5c61167d5f15bafca867d4945
Mateus-Rangel <81487200+Mateus-Rangel@users.noreply.github.com>
2022-07-24 21:18:52 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,127 +1,126 @@
 const carouselIndicators = document.querySelector(".carousel-indicators");
-      const estruturaCarousel = [
-        {
-          img: "img/parafuso_tuf_lok.jpg",
-          alt: "parafuso tuf lok",
-          label: "Serviço de troca de parafusos do DAIT – 081 e DAIT – 083",
-        },
-        {
-          img: "img/grampo_u_1.jfif",
-          alt: "grampo u 1",
-          label:
-            "Foi notado que as abraçadeiras de aço carbono estão muito corroídas necessitando a troca por aço inox",
-        },
-        {
-          img: "img/grampo_u_2.jfif",
-          alt: "grampo u 2",
-          label:
-            "Foi notado que as abraçadeiras de aço carbono estão muito corroídas necessitando a troca por aço inox",
-        },
-        {
-          img: "img/serviço_ajuste.jfif",
-          alt: "serviço de ajuste",
-          label:
-            "Serviço de ajuste nos tampões de 2”, 2 ½” e de 1 ½” na lateral e topo dos contentores metálicos",
-        },
-        {
-          img: "img/tampao_2_1_e_meia_pol.jfif",
-          alt: "tampao 2 1 e meia pol",
-          label:
-            "Serviço de ajuste nos tampões de 2”, 2 ½” e de 1 ½” na lateral e topo dos contentores metálicos",
-        },
-        {
-          img: "img/tri_clamp_6_pol_1.jpg",
-          alt: "tri clamp 6 pol 1",
-          label:
-            "Serviço de ajuste nos tampões de 2”, 2 ½” e de 1 ½” na lateral e topo dos contentores metálicos",
-        },
-        {
-          img: "img/tri_clamp_6_pol_2.jpg",
-          alt: "tri clamp 6 pol 2",
-          label:
-            "Serviço de ajuste nos tampões de 2”, 2 ½” e de 1 ½” na lateral e topo dos contentores metálicos",
-        },
-        {
-          img: "img/tri_clamp_6_pol_3.jfif",
-          alt: "tri clamp 6 pol 3",
-          label:
-            "Serviço de ajuste nos tampões de 2”, 2 ½” e de 1 ½” na lateral e topo dos contentores metálicos",
-        },
-        {
-          img: "img/tri_clamp_6_pol_4.jfif",
-          alt: "tri clamp 6 pol 4",
-          label:
-            "Serviço de ajuste nos tampões de 2”, 2 ½” e de 1 ½” na lateral e topo dos contentores metálicos",
-        },
-        {
-          img: "img/triclamp_2_pol_1_meia_pol.jfif",
-          alt: "triclamp 2 pol 1 e meia pol",
-          label:
-            "Serviço de ajuste nos tampões de 2”, 2 ½” e de 1 ½” na lateral e topo dos contentores metálicos",
-        },
-        {
-          img: "img/boca_de_visita_1.jpg",
-          alt: "boca de visita 1",
-          label: "Limpeza, ajuste e substituição de junta da tampa de visita",
-        },
-        {
-          img: "img/boca_de_visita_2.jfif",
-          alt: "boca de visita 2",
-          label: "Limpeza, ajuste e substituição de junta da tampa de visita",
-        },
-        {
-          img: "img/boca_de_visita_3.jfif",
-          alt: "boca de visita 3",
-          label: "Limpeza, ajuste e substituição de junta da tampa de visita",
-        },
-        {
-          img: "img/reuniao_segurança_1.jfif",
-          alt: "reuniao segurança 1",
-          label:
-            "Reunião pré-operacional ou quando necessitava de alguma alteração com técnico de segurança da Essencis",
-        },
-        {
-          img: "img/reuniao_segurança_2.jfif",
-          alt: "reuniao segurança 2",
-          label:
-            "Reunião pré-operacional ou quando necessitava de alguma alteração com técnico de segurança da Essencis",
-        },
-        {
-          img: "img/valv_4_pol.jfif",
-          alt: "valv 4 pol",
-          label:
-            "Manutenção nas válvulas esferas de 4” sendo realizada a retirada, lixamento e aplicação de Primer na semana anterior (11 a 15/07)",
-        },
-        {
-          img: "img/psvs_1.jpg",
-          alt: "psvs 1",
-          label:
-            "Temos um total de 14 PSVs aguardando instalação sendo 12 feito recertificação no dia 16/05/2022 e 2 no dia 19/05/21",
-        },
-        {
-          img: "img/psvs_2.jfif",
-          alt: "psvs 2",
-          label:
-            "Temos algumas com data de validade vencida (testes feitos em 2020)",
-        },
-        {
-          img: "img/grade.jpg",
-          alt: "grade enferrujada",
-          label:
-            "Algumas grades superiores precisam de tratamento anticorrosão e pintura",
-        },
-        {
-          img: "img/parafusos_enferrujados.jfif",
-          alt: "parafusos enferrujados",
-          label:
-            "Parafusos 3/8” para abraçadeira Tuf-Lok precisam ser substituídas devido a corrosão",
-        },
-      ];
-      let htmlParaInjetarcarouselIndicators = "";
-      for (let i = 0; i < estruturaCarousel.length; i++) {
-        const element = estruturaCarousel[i];
-        if (i == 0) {
-          htmlParaInjetarcarouselIndicators += `
+const estruturaCarousel = [
+  {
+    img: "img/parafuso_tuf_lok.jpg",
+    alt: "parafuso tuf lok",
+    label: "Serviço de troca de parafusos do DAIT – 081 e DAIT – 083",
+  },
+  {
+    img: "img/grampo_u_1.jfif",
+    alt: "grampo u 1",
+    label:
+      "Foi notado que as abraçadeiras de aço carbono estão muito corroídas necessitando a troca por aço inox",
+  },
+  {
+    img: "img/grampo_u_2.jfif",
+    alt: "grampo u 2",
+    label:
+      "Foi notado que as abraçadeiras de aço carbono estão muito corroídas necessitando a troca por aço inox",
+  },
+  {
+    img: "img/serviço_ajuste.jfif",
+    alt: "serviço de ajuste",
+    label:
+      "Serviço de ajuste nos tampões de 2”, 2 ½” e de 1 ½” na lateral e topo dos contentores metálicos",
+  },
+  {
+    img: "img/tampao_2_1_e_meia_pol.jfif",
+    alt: "tampao 2 1 e meia pol",
+    label:
+      "Serviço de ajuste nos tampões de 2”, 2 ½” e de 1 ½” na lateral e topo dos contentores metálicos",
+  },
+  {
+    img: "img/tri_clamp_6_pol_1.jpg",
+    alt: "tri clamp 6 pol 1",
+    label:
+      "Serviço de ajuste no triclamp de 6” e de 2 ½” na lateral e topo dos contentores metálicos",
+  },
+  {
+    img: "img/tri_clamp_6_pol_2.jpg",
+    alt: "tri clamp 6 pol 2",
+    label:
+      "Serviço de ajuste no triclamp de 6” e de 2 ½” na lateral e topo dos contentores metálicos",
+  },
+  {
+    img: "img/tri_clamp_6_pol_3.jfif",
+    alt: "tri clamp 6 pol 3",
+    label:
+      "Serviço de ajuste no triclamp de 6” e de 2 ½” na lateral e topo dos contentores metálicos",
+  },
+  {
+    img: "img/tri_clamp_6_pol_4.jfif",
+    alt: "tri clamp 6 pol 4",
+    label:
+      "Serviço de ajuste no triclamp de 6” e de 2 ½” na lateral e topo dos contentores metálicos",
+  },
+  {
+    img: "img/triclamp_2_pol_1_meia_pol.jfif",
+    alt: "triclamp 2 pol 1 e meia pol",
+    label:
+      "Serviço de ajuste no triclamp de 6” e de 2 ½” na lateral e topo dos contentores metálicos",
+  },
+  {
+    img: "img/boca_de_visita_1.jpg",
+    alt: "boca de visita 1",
+    label: "Limpeza, ajuste e substituição de junta da tampa de visita",
+  },
+  {
+    img: "img/boca_de_visita_2.jfif",
+    alt: "boca de visita 2",
+    label: "Limpeza, ajuste e substituição de junta da tampa de visita",
+  },
+  {
+    img: "img/boca_de_visita_3.jfif",
+    alt: "boca de visita 3",
+    label: "Limpeza, ajuste e substituição de junta da tampa de visita",
+  },
+  {
+    img: "img/reuniao_segurança_1.jfif",
+    alt: "reuniao segurança 1",
+    label:
+      "Reunião pré-operacional ou quando necessitava de alguma alteração com técnico de segurança da Essencis",
+  },
+  {
+    img: "img/reuniao_segurança_2.jfif",
+    alt: "reuniao segurança 2",
+    label:
+      "Reunião pré-operacional ou quando necessitava de alguma alteração com técnico de segurança da Essencis",
+  },
+  {
+    img: "img/valv_4_pol.jfif",
+    alt: "valv 4 pol",
+    label:
+      "Manutenção nas válvulas esferas de 4” sendo realizada a retirada, lixamento e aplicação de Primer na semana anterior (11 a 15/07)",
+  },
+  {
+    img: "img/psvs_1.jpg",
+    alt: "psvs 1",
+    label:
+      "Temos um total de 14 PSVs aguardando instalação sendo 12 feito recertificação no dia 16/05/2022 e 2 no dia 19/05/21",
+  },
+  {
+    img: "img/psvs_2.jfif",
+    alt: "psvs 2",
+    label: "Temos algumas com data de validade vencida (testes feitos em 2020)",
+  },
+  {
+    img: "img/grade.jpg",
+    alt: "grade enferrujada",
+    label:
+      "Algumas grades superiores precisam de tratamento anticorrosão e pintura",
+  },
+  {
+    img: "img/parafusos_enferrujados.jfif",
+    alt: "parafusos enferrujados",
+    label:
+      "Parafusos 3/8” para abraçadeira Tuf-Lok precisam ser substituídas devido a corrosão",
+  },
+];
+let htmlParaInjetarcarouselIndicators = "";
+for (let i = 0; i < estruturaCarousel.length; i++) {
+  const element = estruturaCarousel[i];
+  if (i == 0) {
+    htmlParaInjetarcarouselIndicators += `
             <button
             type="button"
             data-bs-target="#carouselCaptions"
@@ -131,8 +130,8 @@ const carouselIndicators = document.querySelector(".carousel-indicators");
             aria-label="Slide 1"
             ></button>
         `;
-        } else {
-          htmlParaInjetarcarouselIndicators += `
+  } else {
+    htmlParaInjetarcarouselIndicators += `
             <button
             type="button"
             data-bs-target="#carouselCaptions"
@@ -140,17 +139,16 @@ const carouselIndicators = document.querySelector(".carousel-indicators");
             aria-label="Slide ${i + 1}"
             ></button>
             `;
-        }
-      }
+  }
+}
 
-      carouselIndicators.innerHTML = htmlParaInjetarcarouselIndicators;
-      const carouselInner = document.querySelector(`.carousel-inner`);
-      console.log(carouselInner);
-      let htmlParaInjetarcarouselInner = "";
-      for (let i = 0; i < estruturaCarousel.length; i++) {
-        const element = estruturaCarousel[i];
-        if (i == 0) {
-          htmlParaInjetarcarouselInner += `
+carouselIndicators.innerHTML = htmlParaInjetarcarouselIndicators;
+const carouselInner = document.querySelector(`.carousel-inner`);
+let htmlParaInjetarcarouselInner = "";
+for (let i = 0; i < estruturaCarousel.length; i++) {
+  const element = estruturaCarousel[i];
+  if (i == 0) {
+    htmlParaInjetarcarouselInner += `
             <div class="carousel-item active">
             <img
               src=${element.img}
@@ -162,8 +160,8 @@ const carouselIndicators = document.querySelector(".carousel-indicators");
             </div>
           </div>
             `;
-        } else {
-          htmlParaInjetarcarouselInner += `
+  } else {
+    htmlParaInjetarcarouselInner += `
             <div class="carousel-item">
             <img
               src=${element.img}
@@ -175,7 +173,21 @@ const carouselIndicators = document.querySelector(".carousel-indicators");
             </div>
           </div>
             `;
-        }
-      }
+  }
+}
 
-      carouselInner.innerHTML = htmlParaInjetarcarouselInner;
+carouselInner.innerHTML = htmlParaInjetarcarouselInner;
+// .classList.replace("arrowDown", "arrowNormal")
+const cabListas = document.querySelectorAll(".cabLista");
+
+for (let i = 0; i < cabListas.length; i++) {
+  const cablista = cabListas[i];
+  cablista.addEventListener("click", (e) => {
+    const arrow = e.composedPath()[0].firstChild;
+    if (arrow.classList.contains("arrowNormal")) {
+      arrow.classList.replace("arrowNormal", "arrowDown");
+    } else {
+      arrow.classList.replace("arrowDown", "arrowNormal");
+    }
+  });
+}
